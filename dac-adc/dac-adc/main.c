d4b8f3d58a17ff6d7dd44c8933aaed11b4729f5a
Brandon Egger <beggr@uiowa.edu>
2022-11-09 05:27:53 -0600

    lab5 initial commit

---
+++ dac-adc/dac-adc/main.c
@@ -0,0 +1,22 @@
+/*
+ * dac-adc.c
+ *
+ * Created: 11/9/2022 5:26:54 AM
+ * Author : brand
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL // 16 MHZ Clock
+#endif
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
