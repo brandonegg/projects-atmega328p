ea233518ca9afc90ce3c91c7472d4d8c1e188c91
Brandon Egger <beggr@uiowa.edu>
2022-11-09 07:06:20 -0600

    added basic serial communication from atmega328 to PC

--- dac-adc/dac-adc/main.c
+++ dac-adc/dac-adc/main.c
@@ -9,14 +9,50 @@
 #define F_CPU 16000000UL // 16 MHZ Clock
 #endif
 
+#define BAUD_RATE_230400_BPS  103 // BAUD = 9600 | (UBRRn = 16MHz / (16*9600-des. BAUD)) - 1
+
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// START OF UART
+/*
+   The following are utilities for handling RS232 serial communication. Many of these functions
+   are modifications from the following guide:
+   http://www.rjhcoding.com/avrc-uart.php
+ */
+void UART_init(uint16_t ubrr)
+{
+	UBRR0L = (uint8_t)(ubrr & 0xFF); // set baudrate in UBRR
+	UBRR0H = (uint8_t)(ubrr >> 8);
+
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0); // enable the transmitter and receiver
+	UCSR0C = 0x06;
+}
+
+void UART_putc(unsigned char data)
+{
+	while(!(UCSR0A & (1<<UDRE0))); // wait for transmit buffer to be empty
+	UDR0 = data; // load data into transmit register
+}
+
+void UART_puts(char* s)
+{
+	while(*s > 0) UART_putc(*s++); // transmit character until NULL is reached
+}
+// END OF UART
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	unsigned int ubrr = BAUD_RATE_230400_BPS;
+	char data[] = "Hello from ATmega328p  ";
+	
+	UART_init(ubrr);
+	
+	while(1)
+	{
+		UART_puts(data);
+	}
 }
 
