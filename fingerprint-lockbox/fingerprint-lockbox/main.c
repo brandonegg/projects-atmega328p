039687ec4c53a11074d415a9495db384e8f259db
Brandon Egger <beggr@uiowa.edu>
2022-11-30 15:03:27 -0600

    initial commit

---
+++ fingerprint-lockbox/fingerprint-lockbox/main.c
@@ -0,0 +1,18 @@
+/*
+ * fingerprint-lockbox.c
+ *
+ * Created: 11/30/2022 3:01:19 PM
+ * Author : brand
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
